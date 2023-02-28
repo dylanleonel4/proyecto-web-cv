8ab3a543729da4b356af3088151bb18bb8fa6b89
Dylan Moya <dylanleonelmoya@hotmail.com>
2023-02-28 15:23:06 -0300

    primer commit

---
+++ js/funciones.js
@@ -0,0 +1,21 @@
+
+
+document.getElementById('boton_datos').addEventListener('click', function() {
+	document.getElementById	("datos").style.display = 'block';
+
+});
+
+document.getElementById('boton_experiencia').addEventListener('click', function() {
+		document.getElementById	("experiencia").style.display = 'block';
+
+});
+
+document.getElementById('boton_estudios').addEventListener('click', function() {
+	document.getElementById	("Estudios").style.display = 'block';
+
+});
+
+document.getElementById('boton_contacto').addEventListener('click', function() {
+	document.getElementById	("contacto").style.display = 'block';
+
+});
